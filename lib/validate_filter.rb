2357a39b191af0942e39057c9c4e314cfd18ffe0
Jake <github@jake.otherinbox.com>
2010-12-06 12:10:25 -0800

    Tabified and included my mods from html_test_extension.

--- lib/validate_filter.rb
+++ lib/validate_filter.rb
@@ -1,42 +1,55 @@
 module Html
-  module Test
-    class ValidateFilter
-      attr_accessor :request, :response, :params, :validators
-      
-      include ::Test::Unit::Assertions
-      include ::Html::Test::Assertions
+	module Test
+		class ValidateFilter
+			attr_accessor :request, :response, :params, :validators
+			
+			include ::Test::Unit::Assertions
+			include ::Html::Test::Assertions
 
-      def initialize(controller)
-        self.request = controller.request
-        self.response = controller.response
-        self.params = controller.params
-        self.validators = controller.class.validators
-      end
+			def initialize(controller)
+				self.request = controller.request
+				self.response = controller.response
+				self.params = controller.params
+				self.validators = controller.class.validators
+			end
 
-      def validate_page
-        url = request.request_uri
-        return if (!should_validate? || ValidateFilter.already_validated?(url))
-        assert_validates(validators, response.body.strip, url, :verbose => true)
-        ValidateFilter.mark_url_validated(url)
-      end
+			def validate_page
+				url = request.request_uri
+				return if (!should_validate? || ValidateFilter.already_validated?(url))
+#				assert_validates(validators, response.body.strip, url, :verbose => true)
+				assert_validates(validators, response.body.strip, url )
+				ValidateFilter.mark_url_validated(url)
+			end
 
-      def self.already_validated?(url)
-        validated_urls[url]
-      end
+			def self.already_validated?(url)
+				if Html::Test::Validator.revalidate_all
+					false
+				else
+					validated_urls[url]
+				end
+			end
 
-      def self.mark_url_validated(url)
-        validated_urls[url] = true        
-      end
+			def self.mark_url_validated(url)
+				validated_urls[url] = true				
+			end
 
-      def self.validated_urls
-        @validated_urls ||= {}
-      end
+			def self.validated_urls
+				@validated_urls ||= {}
+			end
 
-      # Override this method if you only want to validate a subset of pages
-      def should_validate?
-        response.status =~ /200/ &&
-          (response.headers['Content-Type'] =~ /text\/html/i || response.body =~ /<html/)
-      end      
-    end
-  end
+			# Override this method if you only want to validate a subset of pages
+			def should_validate?
+				response.status =~ /200/ &&
+					(response.headers['Content-Type'] =~ /text\/html/i || response.body =~ /<html/)
+			end			
+
+			#	Used in testing (of html_test_extension plugin)
+			#	to remove the validated_urls hash
+			#	so can test with the same url.
+			def self.clear_validated_urls
+				@validated_urls = {}
+			end
+
+		end
+	end
 end
